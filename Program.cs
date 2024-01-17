f235f6d47f94c426d1d63bdda9f026657166dd24
thenerdic <manniemaverick@gmail.com>
2024-01-17 10:55:53 +0700

    can now view database

--- Program.cs
+++ Program.cs
@@ -5,14 +5,41 @@ class Program {
     static void Main()
     {
        string databasePath = "habit_log.db";
+       string? userInput;
 
        if (!File.Exists(databasePath))
        {
         CreateDatabase(databasePath);
        }
 
-       Console.WriteLine("Habit logger CLI started");
-       Console.ReadLine();
+       MainMenu();
+       userInput = Console.ReadLine();
+
+       switch (userInput)
+       {
+        case "0":
+            break;
+        case "1":
+            // view records
+            Console.Clear();
+            Console.WriteLine("Viewing records...");
+            ViewRecords(databasePath);
+            break;
+        case "2":
+            // insert records
+            Console.WriteLine("Inserting records...");
+            break;
+        case "3":
+            // delete records
+            Console.WriteLine("Deleting records...");
+            break;
+        case "4":
+            // update records
+            Console.WriteLine("Updating records...");
+            break;
+        default:
+            break;
+       }
     }
 
     static void CreateDatabase(string databasePath)
@@ -24,7 +51,7 @@ class Program {
             connection.Open();
 
             string CreateTableQuery = @"
-                CREATE TABLE IF NOT EXISTS Habits (
+                CREATE TABLE IF NOT EXISTS Habit (
                     Id INTEGER PRIMARY KEY AUTOINCREMENT,
                     HabitName TEXT NOT NULL,
                     Quantity INTEGER NOT NULL
@@ -39,5 +66,43 @@ class Program {
         }
 
         Console.WriteLine($"Database created at: {Path.GetFullPath(databasePath)}");
+    }
+
+    static void MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("MAIN MENU\n");
+        Console.WriteLine("What would you like to do?\n");
+        Console.WriteLine("Type 0 to Close Application.");
+        Console.WriteLine("Type 1 to View All Records.");
+        Console.WriteLine("Type 2 to Insert Record.");
+        Console.WriteLine("Type 3 to Delete Record.");
+        Console.WriteLine("Type 4 to Update Record.");
+    }
+
+    static void ViewRecords(string databasePath)
+    {
+        using (SQLiteConnection connection = new SQLiteConnection($"Data Source={databasePath};Version=3;"))
+        {
+            connection.Open();
+
+            // select all records from habits table
+            string selectQuery = "SELECT * FROM Habit";
+
+            using (SQLiteCommand command = new SQLiteCommand(selectQuery, connection))
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                Console.WriteLine("Habit Table:");
+                Console.WriteLine("ID\tHabitName\tQuantity");
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Id"]}\t{reader["HabitName"]}\t\t{reader["Quantity"]}");
+                }
+            }
+
+            connection.Close();
+        }
+
     }
 }
